4cfde28a02cda5e38308a48bb4effc4a17d4c42a
Renny Mwangi <rennymwangi@gmail.com>
2023-10-20 02:17:47 +1030

    Bug* Savebook button not working

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -5,9 +5,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          .populate('books');
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
         return userData;
       }
       throw new AuthenticationError('Not logged in');
