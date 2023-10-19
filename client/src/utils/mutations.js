b3ab7057101057476532667ab67966f656f4867d
Renny Mwangi <rennymwangi@gmail.com>
2023-10-19 20:45:46 +1030

    Added types and mutatiosn to clientSide

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,83 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation LOGIN_USER($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          title
+          description
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation ADD_USER($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          title
+          description
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation SAVE_BOOK($bookData: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation REMOVE_BOOK($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
